fa3b2a1ad70551f834e4853b8d302215d2677bf0
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2019-12-11 16:57:11 +0200

    Add files via upload

---
+++ A. Лиса и змейка.py
@@ -0,0 +1,27 @@
+# n, m = map(int, input().split())
+#
+# x= [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47]
+#
+# for i in range(n):
+#     if i % 2 != 0 and i not in x:
+#         for j in range(m):
+#             if j == (m-1):
+#                 print('#', end='')
+#             else:
+#                 print('.', end='')
+#     elif i % 2 != 0 and i in x:
+#         for j in range(m):
+#             if j == (0):
+#                 print('#', end='')
+#             else:
+#                 print('.', end='')
+#     else:
+#         for j in range(m):
+#             print('#', end='')
+#     print()
+
+
+
+
+
+
